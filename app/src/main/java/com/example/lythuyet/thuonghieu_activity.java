52f105b39242e486153a82886a7c81c976a01edb
khuyen123 <khuyen123@gmail.com>
2021-11-04 01:17:29 +0700

    khuyen commit

--- app/src/main/java/com/example/lythuyet/thuonghieu_activity.java
+++ app/src/main/java/com/example/lythuyet/thuonghieu_activity.java
@@ -3,6 +3,8 @@ package com.example.lythuyet;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,6 +12,8 @@ import android.view.Window;
 
 public class thuonghieu_activity extends AppCompatActivity {
     BottomNavigationView bot_nav;
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,8 @@ public class thuonghieu_activity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.thuonghieu_layout);
         bot_nav =findViewById(R.id.thuonghieu_bot_nav);
+        tabLayout= findViewById(R.id.thuonghieu_tab_layout);
+        viewPager = findViewById(R.id.thuonghieu_view_paper);
         bot_nav.setSelectedItemId(R.id.thuonghieu);
         bot_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -54,5 +60,11 @@ public class thuonghieu_activity extends AppCompatActivity {
                 return false;
             }
         });
+        tabLayout.setupWithViewPager(viewPager);
+        thuonghieu_tab_adapter thuonghieuTabAdapter= new thuonghieu_tab_adapter(getSupportFragmentManager());
+        thuonghieuTabAdapter.addFrag(new thuonghieu_dangsale_frag(),"Đang Sale");
+        thuonghieuTabAdapter.addFrag(new thuonghieu_khaitruong_frag(),"Mới khai trương");
+        thuonghieuTabAdapter.addFrag(new thuonghieu_doanhthu_frag(),"Doanh thu cao");
+        viewPager.setAdapter(thuonghieuTabAdapter);
     }
 }
